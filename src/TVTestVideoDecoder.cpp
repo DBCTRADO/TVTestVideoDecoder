37565cf19320d84cbb703b02c09d3033031cc5d7
DBCTRADO <hdustest@gmail.com>
2022-04-24 23:27:44 +0900

    D3D11 で出力サイズが正しくないことがあるのを修正

--- src/TVTestVideoDecoder.cpp
+++ src/TVTestVideoDecoder.cpp
@@ -746,8 +746,9 @@ HRESULT CTVTestVideoDecoder::Transform(IMediaSample *pIn)
 					Buffer.CopyAttributesFrom(&m_FrameBuffer);
 					hr = pD3D11Decoder->DecodeFrame(&Buffer);
 					if (hr == S_OK && Buffer.m_Buffer[0]) {
-						if (m_fCrop1088To1080 && Buffer.m_Height == 1088) {
-							Buffer.m_Height = 1080;
+						if (Buffer.m_Width >= m_FrameBuffer.m_Width && Buffer.m_Height >= m_FrameBuffer.m_Height) {
+							Buffer.m_Width = m_FrameBuffer.m_Width;
+							Buffer.m_Height = m_FrameBuffer.m_Height;
 						}
 
 						hr = DeliverFrame(&Buffer);
